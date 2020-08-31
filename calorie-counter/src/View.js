aa01c9ddc88eb7fde7320a56e2c7f28d92d04e81
Wilson Neto <wilsonpsn@hotmail.com>
2020-08-31 09:42:52 -0300

    added a title

--- calorie-counter/src/View.js
+++ calorie-counter/src/View.js
@@ -1,10 +1,13 @@
 import hh from 'hyperscript-helpers';
 import { h } from 'virtual-dom';
 
-const { pre } = hh(h);
+const { pre, h1, div } = hh(h);
 
 function view(dispatch, model) {
-  return pre(JSON.stringify(model, null, 2));
+  return div({ className: 'mw6' }, [
+    h1({ className: 'f2 pv2 bb' }, 'Calorie Counter'),
+    pre(JSON.stringify(model, null, 2)),
+  ]);
 }
 
 export default view;
